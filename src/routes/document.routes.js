bfe4533ceb61776247e2e936184a70314366f2f8
Gerardo Quintero Solorio <gera.quinteros@hotmail.com>
2026-09-10 11:47:43 -0700

    Add final document response endpoint, closing the document lifecycle

--- src/routes/document.routes.js
+++ src/routes/document.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const documentController = require('../controllers/document.controller');
 const routingController = require('../controllers/routing.controller');
+const responseController = require('../controllers/response.controller');
 const verifyToken = require('../middlewares/verifyToken');
 const requireRole = require('../middlewares/requireRole');
 const { upload, verifyAndSaveFile } = require('../middlewares/uploadDocument');
@@ -11,5 +12,7 @@ router.get('/', verifyToken, documentController.list);
 router.get('/:id', verifyToken, documentController.getById);
 router.post('/', verifyToken, upload, verifyAndSaveFile, documentController.create);
 router.post('/:id/routing', verifyToken, requireRole('ADMIN'), routingController.addSteps);
+router.patch('/:id/routing/:order/complete', verifyToken, routingController.completeStep);
+router.post('/:id/response', verifyToken, responseController.create);
 
 module.exports = router;
